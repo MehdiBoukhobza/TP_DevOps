59537565be2f7f0b95b301012eab4edacbab22b2
Ihab Bokal <bokalihab@gmail.com>
2024-04-24 09:31:36 +0100

    Premiere modification

---
+++ src/main/java/com/example/tp_devops/TpDevOpsApplication.java
@@ -0,0 +1,14 @@
+package com.example.tp_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpDevOpsApplication.class, args);
+        System.out.println("Une modification de la part de développeur");
+    }
+
+}
